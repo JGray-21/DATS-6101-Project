af2f75ddb2f2abf80aa5fd2cced8e42691470e56
VJGit1 <vjntidesh1099@gmail.com>
2024-10-07 16:57:55 -0400

    Update vaijayanti.r

---
+++ Vaijayanti/vaijayanti.r
@@ -0,0 +1,21 @@
+library(gssr)
+years <- c(1972, 1973, 1974, 1975, 1976, 1977, 1978, 1980, 1982, 1983, 1984, 1985, 
+           1986, 1987, 1988, 1989, 1990, 1991, 1993, 1994, 1996, 1998, 2000, 2002, 
+           2004, 2006, 2008, 2010, 2012, 2014, 2016, 2018)
+vars <- c()
+
+names
+
+for(x in years) {
+  temp <- gss_get_yr(x)
+  vars <- c(vars, colnames(temp))
+}
+
+df <- as.data.frame(table(vars))
+
+# Use table() to get the frequency table
+table<-table(df)
+
+# Create Frequency Table with Proportions
+prob_table <- table / sum(table)	 
+print(prob_table)
